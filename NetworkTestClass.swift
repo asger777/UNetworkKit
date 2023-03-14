e8454cd650842b50c1bf6618438717a972ecc979
asger777 <asger.amraxov@gmail.com>
2023-03-14 13:04:00 +0400

    test class

---
+++ NetworkTestClass.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkTestClass.swift
+//  UNetworkKit
+//
+//  Created by Asgar Amrahov on 14.03.23.
+//
+
+import Foundation
+
+public class NetworkTestClass {
+    public static let instance = NetworkTestClass()
+    
+    func testing() {
+        print(#function, "Successs")
+    }
+}
